dafb1aa78f4a7df42c46b6a8cc3b53a72bd97037
sunjin <changido2888@gmail.com>
2024-02-18 17:54:47 +0900

    [ADD] 폰트설정

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <h1 className="font-UhBeecharming text-6xl">home</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        galmuri: ["UhBeecharming", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
