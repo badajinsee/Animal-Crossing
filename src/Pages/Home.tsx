d1ab5c0702b5868160f81934314b3d892c8db7be
sunjin <changido2888@gmail.com>
2024-02-23 18:58:15 +0900

    json + api 데이터 불러오기

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -9,7 +9,6 @@ const Home = () => {
   useEffect(() => {
     const getData = async () => {
       const data = await Animal();
-      console.log(data);
       setVilger(data);
     };
     getData();
@@ -22,19 +21,10 @@ const Home = () => {
       {vilger &&
         vilger.map((item: Villager, index: number) => (
           <div key={index}>
-            <img src={item.image_url} alt="" />
+            {/* <img src={item.image_url} alt="" /> */}
             <h1 className="text-2xl">{item.name}</h1>
-            <h3 className="text-green">{item.species}</h3>
-            <h3>{item.gender}</h3>
-            <h3>{item.personality}</h3>
-            <h3>{item.birthday_month}</h3>
-            <h3>{item.birthday_day}</h3>
-            <h3>{item.quote}</h3>
             {item.nh_details && (
               <img src={item.nh_details.icon_url} alt="아이콘" />
-            )}
-            {item.nh_details && (
-              <img src={item.nh_details.house_exterior_url} alt="아이콘" />
             )}
             <div>---</div>
           </div>
--- src/utils/Animal.tsx
+++ src/utils/Animal.tsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 import config from "../config";
 
+import koreaAnimalName from "./Villagers.json";
+
+interface animalName {
+  name: string;
+}
+
 export const Animal = async () => {
   try {
     const response = await axios.get(
@@ -12,7 +18,23 @@ export const Animal = async () => {
         },
       }
     );
-    return response.data;
+    // 원래 데이터와 일치하는 이름을 모두 가지고 있는 새 배열 생성
+    const newData = response.data.reduce(
+      (acc: animalName[], res: animalName) => {
+        // JSON 파일에서 동물 이름이 일치하는 항목 찾기
+        const matchedAnimal = koreaAnimalName.find(
+          (animal) => animal.name === res.name
+        );
+        // 일치하는 항목이 있으면 새로운 객체를 배열에 추가
+        if (matchedAnimal) {
+          const newName = matchedAnimal.translations.kRko;
+          acc.push({ ...res, name: newName });
+        }
+        return acc;
+      },
+      []
+    );
+    return newData;
   } catch (e) {
     console.error(e);
   }
