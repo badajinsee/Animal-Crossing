bfa8ec8c8d7e4797fe07ad85269095f8716c426f
sunjin <changido2888@gmail.com>
2024-03-07 00:22:07 +0900

    [Add] FurnitureSearch css 적용 & 코드 수정 else문 작성

--- src/Pages/FurnitureSearch.tsx
+++ src/Pages/FurnitureSearch.tsx
@@ -16,27 +16,47 @@ const FurnitureSearch = () => {
 
   return (
     <>
-      <h1>검색한 "{inputValue}" 입니다</h1>
-      {matchedItems.map(
-        (item: {
-          eng: string;
-          ko: string;
-          imageUrl: string;
-          variationsImg?: string;
-        }) => {
-          const displayImage = item.imageUrl || item.variationsImg;
-          if (displayImage) {
-            return (
-              <Link to={`/FurnitureDetail/${encodeURIComponent(item.eng)}`}>
-                <div key={item.eng}>
-                  <div>{item.ko}</div>
-                  <img src={displayImage} alt={item.ko} />
+      <h1 className="text-2xl border-b-2 my-6 pb-2">
+        검색한 "{inputValue}" 입니다.
+      </h1>
+      <div className="flex flex-wrap gap-3 justify-center ">
+        {matchedItems.map(
+          (item: {
+            eng: string;
+            ko: string;
+            imageUrl: string;
+            variationsImg?: string;
+          }) => {
+            const displayImage = item.imageUrl || item.variationsImg;
+            if (displayImage) {
+              return (
+                <div className=" bg-amber-100 hover:bg-white rounded-lg border-2 border-yellow-300 hover:border-dotted">
+                  <Link to={`/FurnitureDetail/${encodeURIComponent(item.eng)}`}>
+                    <div className="items-center  w-46 h-42" key={item.eng}>
+                      <h1
+                        className={`text-center ${
+                          item.ko.length > 9
+                            ? "text-sm items-center my-1 "
+                            : "text-xl"
+                        }`}
+                      >
+                        {item.ko}
+                      </h1>
+                      <img
+                        className="m-auto"
+                        src={displayImage}
+                        alt={item.ko}
+                      />
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            );
+              );
+            } else {
+              return <div>"알맞은 이미지를 찾을수가 없습니다 !"</div>;
+            }
           }
-        }
-      )}
+        )}
+      </div>
     </>
   );
 };
