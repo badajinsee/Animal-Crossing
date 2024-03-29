498a83f7b67f8fa45cf64fcf1a33fa438b7d38b0
unknown <changido2888@gmail.com>
2024-03-29 21:48:51 +0900

    personality select 추가

--- src/Components/Personality.tsx
+++ src/Components/Personality.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 
 const Personality = () => {
-  const [personalitySelected, setPersonalitySelected] = useState("Jock");
+  const [personalitySelected, setPersonalitySelected] = useState("All");
 
   // 주민성향 불러오기
   const animalPersonality = [...new Set(villagers.map((p) => p.personality))];
@@ -15,8 +15,10 @@ const Personality = () => {
     return animalP;
   });
 
-  //
+
+  // 성격별 유형 option
   const personalityOption = [
+    { value:"All", name:"전체"},
     { value: "Jock", name: "운동광" },
     { value: "Cranky", name: "무뚝뚝" },
     { value: "Peppy", name: "아이돌" },
@@ -26,46 +28,77 @@ const Personality = () => {
     { value: "Snooty", name: "느끼함" },
   ];
 
+  // 성격별 onchange handle
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPersonalitySelected(e.target.value);
   };
 
-  return (
-    <div>
-      <select onChange={handleSelect} value={personalitySelected}>
-        {personalityOption.map((item) => (
-          <option key={item.value} value={item.value}>
-            {item.name}
-          </option>
-        ))}
-      </select>
-      {animalPersonality.map((personality, index) => (
-        <div className="mb-20" key={personality}>
-          <div className="flex justify-center mb-7 text-3xl">
-            <h2>{PersonalityTitle[index]}</h2>
-          </div>
-          <div className="flex flex-wrap justify-center gap-2">
-            {villagers
-              .filter((villager) => villager.personality === personality)
-              .map((villager) => (
-                <Link to={`/Vilager/${villager.name}`} key={villager.name}>
-                  <div className="border hover:border-dotted bg-emerald-50 hover:bg-white">
-                    <h1 className="text-2xl text-center">
-                      {villager.translations.kRko}
-                    </h1>
-                    <img
-                      className="w-24"
-                      src={villager.iconImage}
-                      alt={villager.iconImage}
-                    />
-                  </div>
-                </Link>
-              ))}
-          </div>
-        </div>
+  // 성격별 필터링 함수
+  const filterVillagers = () => {
+    if(personalitySelected === 'All'){
+      return villagers
+    }
+    return villagers.filter(villager => villager.personality === personalitySelected)
+  }
+
+return (
+  <div>
+    <select onChange={handleSelect} value={personalitySelected}>
+      {personalityOption.map((item) => (
+        <option key={item.value} value={item.value}>
+          {item.name}
+        </option>
       ))}
-    </div>
-  );
+    </select>
+
+    {personalitySelected === 'All' ? animalPersonality.map((personality, index) => (
+      <div className="mb-20" key={personality}>
+        <div className="flex justify-center mb-7 text-3xl">
+          <h2>{PersonalityTitle[index]}</h2>
+        </div>
+        <div className="flex flex-wrap justify-center gap-2">
+          {villagers
+            .filter((villager) => villager.personality === personality)
+            .map((villager) => (
+              <Link to={`/Villager/${villager.name}`} key={villager.name}>
+                <div className="border hover:border-dotted bg-emerald-50 hover:bg-white">
+                  <h1 className="text-2xl text-center">
+                    {villager.translations.kRko}
+                  </h1>
+                  <img
+                    className="w-24"
+                    src={villager.iconImage}
+                    alt={villager.iconImage}
+                  />
+                </div>
+              </Link>
+            ))}
+        </div>
+      </div>
+    )) : (
+      <div className="flex flex-wrap item-left justify-center gap-2 mt-10">
+         <div>{personalitySelected}</div>
+        {filterVillagers().map((villager) => (
+          <div className=" justify-center gap-2" key={villager.name}>
+            <Link to={`/Vilager/${villager.name}`}>
+              <div className="border hover:border-dotted bg-emerald-50 hover:bg-white">
+                <h1 className="text-2xl text-center">
+                  {villager.translations.kRko}
+                </h1>
+                <img
+                  className="w-24"
+                  src={villager.iconImage}
+                  alt={villager.iconImage}
+                />
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
+
 };
 
 export default Personality;
