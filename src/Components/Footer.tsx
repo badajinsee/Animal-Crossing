042e28159c5fd809f5f5caf3d27b5331bc60c96e
sunjin <changido2888@gmail.com>
2024-04-04 01:25:48 +0900

    🚑️ footer sticky 모바일부분 수정

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,8 +2,9 @@ const Footer = () => {
   const handleScrollTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
-    <footer className="footer w-full bg-headerColor flex justify-center sticky bottom-0 items-center">
+    <footer className="footer w-full bg-headerColor flex justify-center sticky bottom-0 items-center max-sm:fixed max-sm:min-h-10">
       <div className="container mx-32 max-sm:mx-0">
         <div className="flex justify-center">
           <img
